70df454b04798be67a2cb6e3eb1279b11e5bd253
andreyjgolub739 <andreyjgolub739@gmail.com>
2025-03-23 19:26:08 +0200

    Updates

---
+++ precache-manifest.b9a12752451496e96956038245451635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb12ecf49c224f1e09cb91e10ec7bc02",
+    "url": "/WEB_First_react_app/index.html"
+  },
+  {
+    "revision": "7a359c04e9f7059717d5",
+    "url": "/WEB_First_react_app/static/css/main.8ad24e74.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/WEB_First_react_app/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/WEB_First_react_app/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "7a359c04e9f7059717d5",
+    "url": "/WEB_First_react_app/static/js/main.bf135eea.chunk.js"
+  },
+  {
+    "revision": "394955cb8bdd5cdaec0b",
+    "url": "/WEB_First_react_app/static/js/runtime~main.e175405a.js"
+  }
+]);
